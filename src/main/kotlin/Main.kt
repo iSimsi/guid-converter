8c578ca80eebc01c90310d9614f79e9ef3cb9433
Simon Schädler <simon.schaedler@simworld.ch>
2022-11-20 17:37:25 +0100

    improved cli implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,40 @@
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.optional
-import kotlinx.cli.required
+@file:OptIn(ExperimentalCli::class)
 
-fun sayHello(
-    inputFile: String?,
-    outputFile: String?,
-    inputFormat: String,
-    outputFormat: String,
-    inputString: String?
-) {
-    println("Given inputFile: $inputFile")
-    println("Given outputFile: $outputFile")
-    println("Given inputFormat: $inputFormat")
-    println("Given outputFormat: $outputFormat")
-    println("Given inputString: $inputString")
-}
-
+import kotlinx.cli.*
 
 fun main(args: Array<String>) {
-    val parser = ArgParser("guid.converter")
+    val parser = ArgParser("guid-converter")
 
-    // Arguments
-    val inputFile: String? by parser.argument(ArgType.String, description = "Input file").optional()
-    val outputFile: String? by parser.argument(ArgType.String, description = "Output file").optional()
+    // List conversion
+    class File: Subcommand("file", "Convert a list from input file to output file") {
+        val inputFile: String by argument(ArgType.String, description = "Input file")
+        val outputFile: String by argument(ArgType.String, description = "Output file")
+        val inputFileFormat: String by option(ArgType.Choice(listOf("guid", "hex"), { it }),
+            shortName = "i", description = "Format of the values in the input file"
+        ).required()
 
-    // Options
-    val inputFormat: String by parser.option(
-        ArgType.Choice(listOf("guid", "hex"), { it }),
-        shortName = "i", description = "Input Format"
-    ).required()
-    val outputFormat: String by parser.option(
-        ArgType.Choice(listOf("guid", "hex"), { it }),
-        shortName = "o", description = "Output Format"
-    ).required()
-    val inputString: String? by parser.option(
-        ArgType.String,
-        shortName = "s", description = "Convert a single String"
-    )
+        override fun execute() {
 
+        }
+    }
+
+    // Single conversion
+    class Single: Subcommand("single", "Convert a single String") {
+        val inputString: String by option(ArgType.String,
+            shortName = "s", description = "String to convert"
+        ).required()
+        val inputStringFormat: String by option(ArgType.Choice(listOf("guid", "hex"), { it }),
+            shortName = "i", description = "Format of the values in the input file"
+        ).required()
+
+        override fun execute() {
+
+        }
+    }
+
+    // parse arguments and options
+    val file = File()
+    val single = Single()
+    parser.subcommands(file, single)
     parser.parse(args)
-
-    sayHello(inputFile, outputFile, inputFormat, outputFormat, inputString)
 }
